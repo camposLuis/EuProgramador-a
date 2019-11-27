885022bc3ee6818db2c5866e48e4226b0bd07c4f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-11-27 10:03:21 -0300

    Correcao routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 // import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import Apresentation from './pages/Apresentation';
-// import Principal from './pages/Principal';
-import Principal from './components/TelaTerminal';
+import Principal from './pages/Principal';
+// import Principal from './components/TelaTerminal';
 
 export default (isSigned = false) =>
   createAppContainer(
