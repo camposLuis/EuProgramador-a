0e32d4d1315ef5ec5caa36e75485fc187c34f94f
Leandro94 <reisleandro868@gmail.com>
2019-11-27 11:04:42 -0300

    Arquivos de configuração

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Apresentation from './pages/Apresentation';
 import Principal from './pages/Principal';
+import Lesson from './pages/Lesson';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -10,9 +11,10 @@ export default (isSigned = false) =>
       {
         Apresentation,
         Principal,
+        Lesson,
       },
       {
-        initialRouteName: isSigned ? 'Apresentation' : 'Apresentation',
+        initialRouteName: isSigned ? 'Lesson' : 'Lesson',
       }
     )
   );
